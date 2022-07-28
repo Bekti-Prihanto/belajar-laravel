3d39cf5b48e6725f61028ad4d0986762db4a56f0
Bekti Prihanto <bektiprihanto2001@gmail.com>
2022-07-28 23:09:39 +0700

    Menambahkan beberapa kode di route dan menambahkan beberapa file di view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Bekti Prihanto',
+        'email' => 'bektiprihanto2001@gmail.com',
+        'image' => 'profile.jpeg'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
