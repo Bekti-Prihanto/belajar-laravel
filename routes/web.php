2f728c3e3097e4797b1e9a2521045bd54619a11e
Bekti Prihanto <bektiprihanto2001@gmail.com>
2022-07-29 00:58:46 +0700

    Mencoba menggunakan beberapa syntax blade

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home',
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        'title' => 'About',
         'name' => 'Bekti Prihanto',
         'email' => 'bektiprihanto2001@gmail.com',
         'image' => 'profile.jpeg'
@@ -26,5 +29,58 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'author' => 'Bekti Prihanto',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Maiores rerum, deserunt consequuntur consequatur sint minima eos 
+            debitis voluptatem eaque amet veniam odio cum, 
+            hic possimus vero quasi voluptate! Iure, quis.'
+        ],
+        [
+            'title' => 'Second Post',
+            'slug' => 'second-post',
+            'author' => 'Ivana Intan Paramita',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Maiores rerum, deserunt consequuntur consequatur sint minima eos 
+            debitis voluptatem eaque amet veniam odio cum, 
+            hic possimus vero quasi voluptate! Iure, quis.'
+        ],
+    ];
+    return view('posts', [
+        'title' => 'Posts',
+        'posts' => $blog_posts
+    ]);
+});
+
+Route::get('/post/{slug}', function () {
+    $blog_posts = [
+        [
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'author' => 'Bekti Prihanto',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Maiores rerum, deserunt consequuntur consequatur sint minima eos 
+            debitis voluptatem eaque amet veniam odio cum, 
+            hic possimus vero quasi voluptate! Iure, quis.'
+        ],
+        [
+            'title' => 'Second Post',
+            'slug' => 'second-post',
+            'author' => 'Ivana Intan Paramita',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Maiores rerum, deserunt consequuntur consequatur sint minima eos 
+            debitis voluptatem eaque amet veniam odio cum, 
+            hic possimus vero quasi voluptate! Iure, quis.'
+        ],
+    ];
+
+    $post = collect($blog_posts)->where('slug', request('slug'))->first();
+
+    return view('post', [
+        'title' => 'Posts',
+        'post' => $post
+    ]);
 });
